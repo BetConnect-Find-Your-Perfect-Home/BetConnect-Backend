ce8f86dfb4c599b703ed8f27cf11eff3288bf32a
Etsubdink Gashaw <etsubgashaw3@gmail.com>
2026-04-17 17:53:08 +0300

    middlwares headers validators utils

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import { errorHandler } from './middlewares/errorhandler.js';
+import helmet from 'helmet';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use (errorHandler);
+app.use(helmet());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }));
 app.get('/health', (req, res) => {
     res.status(200).json({ 
         status: 'success',
